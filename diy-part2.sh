a15d8a221e06a6cd1aa884c1a81a662e4c476162
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2020-08-04 18:16:32 +0800

    清除旧版argon主题并拉取最新版

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,22 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 获取luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan package/diy-packages/luci-app-serverchan
+# 获取luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/diy-packages/luci-app-adguardhome
+# 获取luci-app-openclash 编译po2lmo
+#git clone -b master https://github.com/vernesong/OpenClash package/openclash
+#pushd package/openclash/luci-app-openclash/tools/po2lmo
+#make && sudo make install
+#popd
+#=================================================
+# 清除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+# 清除旧版argon主题并拉取最新版
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
